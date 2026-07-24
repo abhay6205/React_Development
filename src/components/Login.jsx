1d481a5bb26f266a70c86cb1a7bca27afee685b9
ABHAY KUMAR <abhay11082005@gmail.com>
2026-07-24 10:37:11 +0530

    adding login form in this react web application

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+function Login() {
+    const isLogin = true
+
+    if(isLogin) {
+        return <h1>Welcome, You are Signed In</h1>
+    } else {
+        return <h1>Login First</h1>
+    }
+}
+
+export default Login
