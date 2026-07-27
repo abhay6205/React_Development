d2bd694ede9a28da05111f8dee8db89b7fb18cf2
ABHAY KUMAR <abhay11082005@gmail.com>
2026-07-27 21:09:08 +0530

    adding tailwind css for better ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 function Navbar() {
   return (
-    <>
-      <h1>Navbar</h1>
-    </>
+    <nav className="bg-blue-600 text-white flex justify-between items-center px-6 py-4 mb-2">
+      <h1 className="text-3xl font-bold my-2">Student Dashboard</h1>
+
+      <div className="flex gap-4">
+        <a href="#" className="font-bold text-3xl hover:bg-blue-300">Home</a>
+        <a href="#" className="font-bold text-3xl hover:bg-blue-300">About</a>
+        <a href="#" className="font-bold text-3xl hover:bg-blue-300">Login</a>
+      </div>
+    </nav>
   );
 }
 export default Navbar
